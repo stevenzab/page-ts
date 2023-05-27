35a03cb5c9744e7d45e362bbc865a12038aa27b0
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-05-27 20:03:58 +0200

    [ADD] logo and resize the img

--- testex/src/components/box_info.tsx
+++ testex/src/components/box_info.tsx
@@ -2,20 +2,36 @@ import React from 'react'
 import '../styles/box_info.css'
 
 type BoxProps = {
-    title: string;
-    subtitle: string;
-    text: string;
-    icon?: React.ReactNode;
+	title: string;
+	subtitle: string;
+	text: string;
+	icon?: React.ReactNode;
+	icon2?: React.ReactNode;
+	icon3?: React.ReactNode;
+	titreicon1?: string;
+	titreicon2?: string;
+	titreicon3?: string;
+	logo?: React.ReactNode | string;
 }
 
 const Box = (props: BoxProps) => {
-    return (
-        <div className='container-box'>{props.title}
-            <div className='container-box-subtitle'>{props.subtitle}</div>
-            <div className='container-box-text'>{props.text}</div>
-            <div className='container-box-icon'>{props.icon}</div>
-        </div>
-    )
+	return (
+		<div className='container-box'>
+			<div className='container-box-logo'>
+				<div className='container-box-logo-img'>
+					{props.logo}
+				</div>
+				{props.title}
+			</div>
+			<div className='container-box-subtitle'>{props.subtitle}</div>
+			<div className='container-box-text'>{props.text}</div>
+			<div className='container-box-icon'>
+				{props.icon} {props.titreicon1}
+				{props.icon2} {props.titreicon2}
+				{props.icon3} {props.titreicon3}
+			</div>
+		</div>
+	);
 }
 
 export default Box
--- testex/src/index.tsx
+++ testex/src/index.tsx
@@ -8,6 +8,11 @@ import SearchBar from './components/searchbar';
 import Box from './components/box_info';
 import reportWebVitals from './reportWebVitals';
 import { HiBriefcase } from 'react-icons/hi';
+import { TfiWorld } from 'react-icons/tfi';
+import { SiAdguard } from 'react-icons/si';
+import CoopCycle from './assets/coopcycle.png';
+import Polere from './assets/polère.png';
+import OpenFoodFact from './assets/food.png';
 
 
 const root = ReactDOM.createRoot(
@@ -19,9 +24,32 @@ root.render(
       <Header />
       <Card title='Explorer' text='Vous pouvez répondre à une demande d’entreprise, effectuer une offre d’apport et intégrer plusieurs projets à la fois.' />
       <SearchBar />
-      <Box title='CoopCycle' subtitle='app.federage.com/coopcycle' text='Fédération de coopératives de livraison à vélo, gouvernée démocratiquement et orientée mutualisation des services.' icon=<HiBriefcase /> />
-      <Box title='OpenFoodFacts' subtitle='app.federage.com/openfoodfacts' text='Projet collaboratif de base de données libre et ouverte sur les produits alimentaires commercialisés dans...' icon=<HiBriefcase /> />
-      <Box title='Polère' subtitle='app.federage.com/polerebrand' text='Spécialiste de la conception et de la vente de vêtements éco-responsables, durables et éthiques.' icon=<HiBriefcase /> />
+      <Box title='CoopCycle'
+        subtitle='app.federage.com/coopcycle'
+        logo={<img src={CoopCycle} alt="CoopCycle Logo" />}
+        text='Fédération de coopératives de livraison à vélo, gouvernée démocratiquement et orientée mutualisation des services.'
+        icon=<HiBriefcase />
+        titreicon1='Projets ouverts'
+        icon2=<TfiWorld />
+        titreicon2='Coopérative'
+        icon3=<SiAdguard />
+        titreicon3='Vérifié' />
+      <Box title='OpenFoodFacts'
+        subtitle='app.federage.com/openfoodfacts'
+        logo={<img src={OpenFoodFact} alt="OpenFoodFacts Logo" />}
+        text='Projet collaboratif de base de données libre et ouverte sur les produits alimentaires commercialisés dans...'
+        icon=<HiBriefcase />
+        titreicon1='Projets ouverts'
+        icon2=<TfiWorld />
+        titreicon2='Commun' />
+      <Box title='Polère'
+        subtitle='app.federage.com/polerebrand'
+        logo={<img src={Polere} alt="Polere Logo" />}
+        text='Spécialiste de la conception et de la vente de vêtements éco-responsables, durables et éthiques.'
+        icon=<HiBriefcase />
+        titreicon1='Projets ouverts'
+        icon2=<TfiWorld />
+        titreicon2='Entreprise' />
       <Footer />
     </div>
   </React.StrictMode>
